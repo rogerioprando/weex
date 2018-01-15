059cce46a9516eab1bb50feb7e8edda31a7ab9ce
Rogerio Prando <rogerio.prando@wplex.com.br>
2018-01-15 17:52:11 -0200

    Using Jinja templates.
    Signed-off-by: Rogerio Prando <rogerio.prando@wplex.com.br>

--- app/views/contact.py
+++ app/views/contact.py
@@ -3,6 +3,7 @@ from flask import Blueprint, render_template
 mod = Blueprint('contact', __name__, url_prefix='/contact')
 
 
+@mod.route('/', defaults={'page': 'contato'})
 @mod.route('/<page>')
 def contact(page):
     return render_template('contact/%s.html' % page)
--- app/views/home.py
+++ app/views/home.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, render_template
 
-mod = Blueprint('home', __name__, url_prefix='/home')
+mod = Blueprint('home', __name__, url_prefix='/home', template_folder='templates')
 
 
+@mod.route('/', defaults={'page': 'index'})
 @mod.route('/<page>')
 def index(page):
-    return render_template('home/%s.html' % page)
+        return render_template('home/%s.html' % page)
 
 
 @mod.route('/<page>')
