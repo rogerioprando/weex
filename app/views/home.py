9f874ca4f7a82d4730cccccf917b4f8e9f3799cc
Rogerio Prando <rogerio.prando@wplex.com.br>
2018-01-16 17:00:22 -0200

    organized static folder and started tests with devices and xvm page.
    Signed-off-by: Rogerio Prando <rogerio.prando@wplex.com.br>

--- app/views/home.py
+++ app/views/home.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 
+
 mod = Blueprint('home', __name__, url_prefix='/home', template_folder='templates')
 
 
@@ -12,6 +13,8 @@ def index(page):
 @mod.route('/<page>')
 def news(page):
     return render_template('home/%s.html' % page)
+
+
 
 
 
--- app/views/remote.py
+++ app/views/remote.py
@@ -1,13 +1,16 @@
 from flask import Blueprint, render_template
+from app.devices import *
 
 mod = Blueprint('remote', __name__, url_prefix='/remote')
 
 
 @mod.route('/<page>')
 def xvm(page):
-    return render_template('remote/%s.html' % page)
+    return render_template('remote/%s.html' % page, value=getevent())
 
 
 @mod.route('/<page>')
 def programacao(page):
     return render_template('remote/%s.html' % page)
+
+
